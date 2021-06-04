fb2ae5cc524d8d19275799c4b1e5fd7e6a958db7
beniteza <85143152+beniteza@users.noreply.github.com>
2021-06-04 17:16:38 -0400

    Created Login page

--- AngularUI/src/app/components/home/login/login.component.ts
+++ AngularUI/src/app/components/home/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NotifierService } from 'angular-notifier';
+import { HomeService } from 'src/app/services/home.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formModel: FormGroup;
+
+  constructor(private fb: FormBuilder, private service: HomeService, private router: Router, private notifier: NotifierService) { }
 
   ngOnInit(): void {
+    if (localStorage.getItem('token') != null)
+      this.router.navigateByUrl('/petition/list');
+
+    this.formModel = this.fb.group({
+      UserName: ['', Validators.required],
+      Password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.service.login(this.formModel.value).subscribe(
+      (res: any) => {
+        localStorage.setItem('token', res.token);
+        this.notifier.notify('success', 'Login successful');
+        this.router.navigateByUrl('/petition/list');
+      },
+      err => {
+        if (err.status == 400)
+          this.notifier.notify('error', 'Incorrect username or password. Authentication failed.');
+        else{
+          this.notifier.notify('error', 'Error: Something went wrong!');
+          console.log(err);
+        }
+      }
+    );
   }
 
 }
